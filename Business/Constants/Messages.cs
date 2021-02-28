d356d1768a49003257bdf3a19773af5471261021
Fatma Betül Uyar <fbluyar2@gmail.com>
2021-02-28 16:35:02 +0300

    wep API

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    //newlemeye gerek yok
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductListed = "Ürünler listelendi";
+    }
+}
---
+++ Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessResult:Result
+    {
+        public SuccessResult(string message) : base(true,message)  //base in iki parametreli olanını kullan
+        {
+
+        }
+
+        public SuccessResult() : base(true)
+        {
+
+        }
+    }
+}
